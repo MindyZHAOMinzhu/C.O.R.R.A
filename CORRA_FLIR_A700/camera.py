7ac637a0f9b540543848e84cd47ac7dbe14d30d7
MindyZHAOMinzhu <zhaominzhumindy@gmail.com>
2025-01-16 15:49:35 -0600

    add some explainations

--- CORRA_FLIR_A700/camera.py
+++ CORRA_FLIR_A700/camera.py
@@ -1,25 +1,38 @@
+# Defining "FLIRCamera" class
+# initialization and setup of the camera
+
+
+# Usage Examples
+# camera = FLIRCamera()  # Initialize the camera
+# frame = camera.acquire_frame()  # Acquire a single frame (frame.shape [shape of the image NumPy array format])
+# camera.release()  # Ensure proper cleanup
+
 import PySpin
 
 class FLIRCamera:
+    # Initialization
     def __init__(self):
         self.system = PySpin.System.GetInstance()
         self.cam_list = self.system.GetCameras()
-        self.cam = self.cam_list.GetByIndex(0)
-        self.cam.Init()
-        self.cam.BeginAcquisition()
+        self.cam = self.cam_list.GetByIndex(0) # Selects the first camera in the list
+        self.cam.Init() # Initializes the selected camera for use.
+        self.cam.BeginAcquisition() # Starts the camera's image acquisition process, enabling frame capture.
 
-    def acquire_frame(self):
-        image_result = self.cam.GetNextImage()
+
+3   def acquire_frame(self):
+        image_result = self.cam.GetNextImage() # store captured the next image from the camera's acquisition stream
+        
+        # check if the image_result
         if image_result.IsIncomplete():
             print("Image incomplete with image status %d ..." % image_result.GetImageStatus())
             return None
-        else:
-            image_data = image_result.GetNDArray()
-            image_result.Release()
+        else: # if the image_result is complete
+            image_data = image_result.GetNDArray() # converting image to NumPy array format for further analysis
+            image_result.Release() # after converting this image to NumPy array, releasing the acquired image from the camera's memory
             return image_data
 
-    def release(self):
-        self.cam.EndAcquisition()
+    def release(self): # for the camera, Cleanup
+        self.cam.EndAcquisition() # Stops the camera's acquisition process.
         self.cam.DeInit()
-        self.cam_list.Clear()
+        self.cam_list.Clear() # Clears the list of connected cameras
         self.system.ReleaseInstance()
--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # C.O.R.R.A
 
 
+## What it is and for what?
+
 ## How to Run / Study Replication
 
 ### Prerequisites
@@ -15,3 +17,8 @@
 ### FLIR A700
 
 [FLIR A700](https://www.flir.com/products/a400-a700-science-kits/?model=85903-0202&vertical=rd+science&segment=solutions) is a fixed entry-level thermal cameras from [Teledyne FLIR](https://www.flir.com/), which provides accurate temperature measurement. We could easily view, acquire, and analyze data using included FLIR Research Studio software. 
+
+
+### PySpin
+
+PySpin is a Python library for interfacing with FLIR cameras, providing access to the Spinnaker SDK's camera control features.
