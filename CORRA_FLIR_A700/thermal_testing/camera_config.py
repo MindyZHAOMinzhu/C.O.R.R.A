0879eea4b1a70442ca4847a96a7169bfb0ec9eb6
MindyZHAOMinzhu <zhaominzhumindy@gmail.com>
2025-01-16 16:21:12 -0600

    Update camera_config.py

--- CORRA_FLIR_A700/thermal_testing/camera_config.py
+++ CORRA_FLIR_A700/thermal_testing/camera_config.py
@@ -1,13 +1,15 @@
-import PySpin
+import PySpin # Explanation about PySpin more: PySpin library provides access to FLIR camera settings and functionalities
 
+# Function is created to configure the camera for thermal imaging
 def configure_camera_for_thermal(camera):
     nodemap = camera.GetNodeMap()
     pixel_format = PySpin.CEnumerationPtr(nodemap.GetNode('PixelFormat'))
     if not PySpin.IsAvailable(pixel_format) or not PySpin.IsWritable(pixel_format):
-        print("Unable to set Pixel Format to Mono16.")
+        print("Unable to set Pixel Format to Mono16.") # Mono16 is 16bit monochrome encoding
         return False
 
     pixel_format_mono16 = pixel_format.GetEntryByName('Mono16')
     pixel_format.SetIntValue(pixel_format_mono16.GetValue())
     print("Pixel Format set to Mono16 for thermal imaging.")
-    return True
+    
+    return True 
